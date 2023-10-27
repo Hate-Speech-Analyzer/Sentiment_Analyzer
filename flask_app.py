ce8f217fb79d9304fe10bd7c759f61c2ab607c18
Khadijatul Kobra <ktk_01@outlook.com>
2023-10-27 14:05:29 +0300

    updates app.py

--- flask_app.py
+++ flask_app.py
@@ -34,7 +34,9 @@ def result():
     print("RESULT IS" , resData)
     return render_template('index.html' , result=resData )
 
+# Get the port from the PORT environment variable, default to 5000 if not set
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=port)
 
